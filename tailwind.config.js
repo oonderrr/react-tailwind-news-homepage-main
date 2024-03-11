663cbb8aedcb6cf2b778a35ab3a8c9ac6c7d6d59
onder <onder.bugdaci@gmail.com>
2024-03-11 15:02:44 +0300

    gerekli fontlar ve renkler eklendi

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+
+    fontFamily: {
+      'inter': ["Inter", "sans-serif"],
+    },
+
+    extend: {
+      colors: {
+        "p-soft-orange": "hsl(35, 77%, 62%)",
+        "p-soft-red": "hsl(5, 85%, 63%)",
+        "p-off-white": "hsl(36, 100%, 99%)",
+        "p-grayish-blue": "hsl(233, 8%, 79%)",
+        "p-dark-grayish-blue": "hsl(236, 13%, 42%)",
+        "p-very-dark-blue": "hsl(240, 100%, 5%)"
+      },
+    },
   },
   plugins: [],
 }
